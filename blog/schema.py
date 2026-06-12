a47df3f8e2af9e24b4753608ebb5976f88f3bb47
C S Sanath Sreekumar <165697113+sanath2003git@users.noreply.github.com>
2026-06-12 23:18:55 +0530

    Add GraphQL endpoint and execute first query

--- blog/schema.py
+++ blog/schema.py
@@ -8,3 +8,14 @@ class BookType(DjangoObjectType):
 
     class Meta:
         model = Book
+
+
+class Query(graphene.ObjectType):
+
+    books = graphene.List(BookType)
+
+    def resolve_books(self, info):
+        return Book.objects.all()
+
+
+schema = graphene.Schema(query=Query)
