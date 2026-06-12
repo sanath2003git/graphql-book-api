91715a22250b3d1554ed5b8fa6f16c40172dd209
C S Sanath Sreekumar <165697113+sanath2003git@users.noreply.github.com>
2026-06-12 22:50:16 +0530

    Install and configure Graphene-Django

--- core/settings.py
+++ core/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     "blog",
+    "graphene_django",
 ]
 
 MIDDLEWARE = [
@@ -117,3 +118,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+GRAPHENE = {
+    "SCHEMA": "blog.schema.schema"
+}
