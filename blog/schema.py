4e39a3ada0a4c6f1dd533f259c9bafe82f933dae
C S Sanath Sreekumar <165697113+sanath2003git@users.noreply.github.com>
2026-06-13 12:25:05 +0530

    Implement single book query with GraphQL

--- blog/schema.py
+++ blog/schema.py
@@ -14,8 +14,20 @@ class Query(graphene.ObjectType):
 
     books = graphene.List(BookType)
 
+    book = graphene.Field(
+        BookType,
+        id=graphene.Int(required=True)
+    )
+
     def resolve_books(self, info):
         return Book.objects.all()
+    
+    def resolve_book(self, info, id):
+        try:
+            return Book.objects.get(id=id)
+
+        except Book.DoesNotExist:
+            return None
 
 
 schema = graphene.Schema(query=Query)
